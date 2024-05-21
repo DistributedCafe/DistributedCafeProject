24cca73c5e0d20489c556d9ab5cc80c974edef8f
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-21 11:18:45 +0200

    chore: add dokka, ktlint and jacoco to WarehouseService module

--- WarehouseService/build.gradle.kts
+++ WarehouseService/build.gradle.kts
@@ -1,5 +1,12 @@
 plugins {
     alias(libs.plugins.kotlin)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.ktlint)
+    id("jacoco")
+}
+
+jacoco {
+    toolVersion = libs.versions.jacoco.get()
 }
 
 repositories {
@@ -8,8 +15,19 @@ repositories {
 
 dependencies {
     testImplementation(libs.bundles.kotlin.testing)
+    testImplementation(libs.bundles.cucumber.testing)
+    testImplementation(libs.junit.vintage)
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+    }
 }
