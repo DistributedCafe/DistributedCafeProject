7bca02cd6527dc0eafd0b612028ca71fedd5aa3f
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-27 12:44:12 +0200

    refactor: code formatting

--- WarehouseService/src/main/kotlin/server/Main.kt
+++ WarehouseService/src/main/kotlin/server/Main.kt
@@ -6,7 +6,6 @@ import io.vertx.core.Vertx
  * Entry point of the service
  */
 object Main {
-
     @JvmStatic
     fun main(args: Array<String>) {
         val vertx = Vertx.vertx()
--- WarehouseService/src/main/kotlin/server/Server.kt
+++ WarehouseService/src/main/kotlin/server/Server.kt
@@ -3,15 +3,13 @@ package server
 import io.vertx.kotlin.core.http.httpServerOptionsOf
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 
-
 class Server : CoroutineVerticle() {
-
     override suspend fun start() {
         vertx.createHttpServer(
             httpServerOptionsOf(
                 port = 8080,
-                host = "localhost"
-            )
+                host = "localhost",
+            ),
         )
             .requestHandler { req ->
                 req.response().end("Warehouse service")
