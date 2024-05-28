3cb4eaba92bceb650768442e9cb2c87e996590a1
Elisa Albertini <elisa.albertini25@gmail.com>
2024-05-28 10:37:17 +0200

    docs: add documentation

--- WarehouseService/src/test/kotlin/repository/RunCucumberTest.kt
+++ WarehouseService/src/test/kotlin/repository/RunCucumberTest.kt
@@ -4,6 +4,9 @@ import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
 
+/**
+ * Class that runs cucumber tests
+ */
 @RunWith(Cucumber::class)
 @CucumberOptions(
     features = ["src/test/kotlin/repository/features"],
