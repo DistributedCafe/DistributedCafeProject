d1585cbad58801ff41d682f60d98e9d1d4b3a14c
Elisa Albertini <elisa.albertini25@gmail.com>
2024-06-26 16:46:34 +0200

    chore: load data from db

--- manager-application/src/app/data-table/table.component.ts
+++ manager-application/src/app/data-table/table.component.ts
@@ -1,18 +1,14 @@
 import {Component} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
+import { RequestMessage, ResponseMessage, WarehouseServiceMessages } from '../../utils/messages';
+import { Service } from '../../utils/service';
+import { CommonModule } from '@angular/common';
 
 //TODO importa da server (?)
 export interface Ingredient {
   name: string;
   quantity: number;
 }
-
-const ELEMENT_DATA: Ingredient[] = [
-  {name: 'Milk', quantity: 100},
-  {name: 'Coffe', quantity: 9},
-  {name: 'Kiwi', quantity: 70},
-];
-
 /**
  * @title Basic use of `<table mat-table>`
  */
@@ -21,9 +17,40 @@ const ELEMENT_DATA: Ingredient[] = [
   styleUrl: 'table.component.css',
   templateUrl: 'table.component.html',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule,
+    CommonModule
+  ],
 })
 export class TableComponent {
   displayedColumns: string[] = ['name', 'quantity'];
-  dataSource = ELEMENT_DATA;
+  display = false
+  dataSource: Ingredient[] = []
+ 
+  constructor(){
+    let ws: WebSocket;
+    let ingredients: Ingredient[]
+    const initialRequest: RequestMessage = {
+      client_name: Service.WAREHOUSE,
+	    client_request: WarehouseServiceMessages.GET_ALL_INGREDIENT,
+	    input: ''
+    }
+
+    ws = new WebSocket('ws://localhost:3000');
+    ws.onopen = function() {
+      console.log("Websocket opend!")
+      ws.send(JSON.stringify(initialRequest))
+    }
+
+    ws.onmessage = function(e){
+      const data = JSON.parse(e.data) as ResponseMessage
+      console.log("message: " + data.message)
+      console.log("code: " + data.code)
+      ingredients = JSON.parse(data.data) as Ingredient []
+      setData(ingredients, true)
+    }
+    const setData = (ingredients: Ingredient[], display: boolean) =>{
+      this.dataSource = ingredients
+      this.display = display
+    }
+  }
 }
