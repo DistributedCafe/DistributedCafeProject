e23f4088bedb37f10092b46b1d5c516824b143b1
Elisa Albertini <elisa.albertini25@gmail.com>
2024-06-29 09:12:54 +0200

    fix: send error message to client when microservice is not available

--- server/src/check-service.ts
+++ server/src/check-service.ts
@@ -3,10 +3,11 @@ import { Service } from './utils/service';
 import axios from 'axios';
 import { fromStringToInput, fromStringToArrayInput } from './string-utils';
 
+console.log("sono vivo")
 const http = axios.create({
 	baseURL: 'http://localhost:8080'
-});
-
+})
+console.log("sono ancora vivo")
 /**
  * This function is used to call the correct microservice and API based on the received RequestMessage. 
  * It also sends the answer back through the websocket
@@ -61,12 +62,20 @@ function handleResponse(promise: Promise<any>, ws: WebSocket) {
 		}
 		ws.send(JSON.stringify(msg))
 	}).catch((error) => {
-		const msg: ResponseMessage = {
-			message: error.response.statusText,
-			code: error.response.status,
-			data: ""
+		var msg: ResponseMessage
+		if (error.response == undefined) {
+			msg = {
+				message: "ERROR_SERVER_NOT_AVAILABLE",
+				code: 500,
+				data: ""
+			}
+		} else {
+			msg = {
+				message: error.response.statusText,
+				code: error.response.status,
+				data: ""
+			}
 		}
 		ws.send(JSON.stringify(msg))
 	});
-
 }
