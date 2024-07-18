9baed2e7237d27cd2fdac0a0f30a557621ce8f6e
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2024-07-18 11:59:06 +0200

    chore: add script in orders service and test separation

--- server/test/test-order.test.ts
+++ server/test/test-order.test.ts
@@ -8,8 +8,8 @@ import express from "express"
 import { IncomingMessage, Server, ServerResponse, createServer } from 'http';
 import { check_service } from '../src/check-service';
 
-let ws1: WebSocket
-let ws: WebSocket;
+let ws_route: WebSocket
+let ws_check_service: WebSocket;
 let wss: WebSocketServer;
 let server: Server<typeof IncomingMessage, typeof ServerResponse>
 let insertedId: string
@@ -19,8 +19,8 @@ beforeAll(async () => {
 	await cleanCollection("Orders", "Orders")
 	await cleanCollection("Menu", "Items")
 	await cleanCollection("Warehouse", "Ingredient")
-	let warehause = await getCollection("Warehouse", "Ingredient")
-	await warehause.createIndex({ name: 1 }, { unique: true })
+	let warehouse = await getCollection("Warehouse", "Ingredient")
+	await warehouse.createIndex({ name: 1 }, { unique: true })
 	let menu = await getCollection("Menu", "Items")
 	await menu.createIndex({ name: 1 }, { unique: true })
 	let res = await add("Orders", "Orders", JSON.stringify(order))
@@ -37,8 +37,12 @@ beforeEach(async () => {
 })
 
 afterEach(() => {
-	ws.close()
-	ws1.close()
+	if (ws_check_service !== undefined && ws_check_service.OPEN) {
+		ws_check_service.close()
+	}
+	if (ws_route !== undefined && ws_route.OPEN) {
+		ws_route.close()
+	}
 	server.close()
 })
 
@@ -48,8 +52,12 @@ afterAll(() => { closeMongoClient() })
 test('Get all orders - 200', done => {
 	let requestMessage = createRequestMessage(Service.ORDERS, OrdersServiceMessages.GET_ALL_ORDERS.toString(), '')
 	startWebsocket(requestMessage, 200, "OK", addId(order, insertedId), done)
-	createConnectionAndCall(requestMessage, 200, "OK", addId(order, insertedId), done)
 });
+
+test('Get all orders - 200 (check-service)', done => {
+	let requestMessage = createRequestMessage(Service.ORDERS, OrdersServiceMessages.GET_ALL_ORDERS.toString(), '')
+	createConnectionAndCall(requestMessage, 200, "OK", addId(order, insertedId), done)
+})
 
 //write
 test('Create Order Test - 200', done => {
@@ -91,18 +99,23 @@ test('Create Order Test 2 - 200', done => {
 test('Create Order Test - 400', done => {
 	let requestMessage = createRequestMessage(Service.ORDERS, OrdersServiceMessages.CREATE_ORDER.toString(), newWrongOrder)
 	startWebsocket(requestMessage, 400, "ERROR_WRONG_PARAMETERS", "", done)
-	createConnectionAndCall(requestMessage, 400, "ERROR_WRONG_PARAMETERS", "", done)
+
 });
 
+test('Create Order Test - 400 (check-service)', done => {
+	let requestMessage = createRequestMessage(Service.ORDERS, OrdersServiceMessages.CREATE_ORDER.toString(), newWrongOrder)
+	createConnectionAndCall(requestMessage, 400, "ERROR_WRONG_PARAMETERS", "", done)
+})
+
 function startWebsocket(requestMessage: RequestMessage, code: number, message: string, data: any, callback: jest.DoneCallback) {
-	ws1 = new WebSocket('ws://localhost:3000');
-	ws1.on('message', async (msg: string) => {
+	ws_route = new WebSocket('ws://localhost:3000');
+	ws_route.on('message', async (msg: string) => {
 		await check_order_message(JSON.parse(msg), code, message, data, requestMessage.client_request)
 		callback()
 	});
 
-	ws1.on('open', () => {
-		ws1.send(JSON.stringify(requestMessage))
+	ws_route.on('open', () => {
+		ws_route.send(JSON.stringify(requestMessage))
 	});
 }
 
@@ -118,8 +131,8 @@ function createConnectionAndCall(requestMessage: RequestMessage, code: number, m
 	});
 	server.listen(8081, () => console.log('listening on port :8081'));
 
-	ws = new WebSocket('ws://localhost:8081');
-	ws.on('open', () => {
-		check_service(requestMessage, ws)
+	ws_check_service = new WebSocket('ws://localhost:8081');
+	ws_check_service.on('open', () => {
+		check_service(requestMessage, ws_check_service)
 	})
 }
