b6d838910eedce4a6e0ac8bd1f7802d6b2e7cecb
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2024-07-19 20:54:32 +0200

    chore: add repository functionalities and tests

--- orders-service/src/repository/repository.ts
+++ orders-service/src/repository/repository.ts
@@ -2,6 +2,7 @@ import { OrderType, OrderItem, OrderState, Order } from '../domain/order'
 import { OrdersMessage } from "../orders-message";
 import * as mongoConnection from "./connection"
 import { MongoOrder, toInsertOrder, fromMongoOrderToOrder } from './order-conversion-utils';
+import { ObjectId } from "mongodb"
 
 /**
  * This type represents the Response given by the repository. It consists of the generic data and an OrdersMessage
@@ -47,3 +48,40 @@ export async function getAllOrders(): Promise<RepositoryResponse<Order[]>> {
 		return { data: orders, message: OrdersMessage.EMPTY_ORDERS_DB };
 	}
 }
+
+export async function findOrderById(orderId: string): Promise<RepositoryResponse<Order>>{
+	if (!ObjectId.isValid(orderId)){
+		return {data: undefined, message: OrdersMessage.ORDER_ID_NOT_FOUND}
+	}
+
+	let ordersCollection = await collection
+	let id = new ObjectId(orderId)
+	let find = (await ordersCollection.find({"_id": id}).toArray())
+	if (find.length == 0){
+		return {data: undefined, message: OrdersMessage.ORDER_ID_NOT_FOUND}
+	}
+	let mongoOrder = find.at(0) as MongoOrder
+	let order = fromMongoOrderToOrder(mongoOrder._id, mongoOrder.customerContact, mongoOrder.price, mongoOrder.type, mongoOrder.state, mongoOrder.items)
+	return {data: order, message: OrdersMessage.OK}
+	
+}
+
+export async function updateOrder(orderId: string, newState: OrderState): Promise<RepositoryResponse<Order>>{
+	if(!ObjectId.isValid(orderId)){
+		return {data: undefined, message: OrdersMessage.ORDER_ID_NOT_FOUND}
+	}
+	let filter = { "_id" : new ObjectId(orderId)}
+	let update = { $set: { "state" : newState } }
+	
+	let ordersCollection = await collection
+	let res = await ordersCollection.updateOne(filter, update)
+	if(res.modifiedCount == 1){
+		let order = (await findOrderById(orderId)).data
+		return {data: order, message: OrdersMessage.OK}
+	}else{
+		return {data: undefined, message: OrdersMessage.ORDER_ID_NOT_FOUND}
+	}
+
+}
+
+
