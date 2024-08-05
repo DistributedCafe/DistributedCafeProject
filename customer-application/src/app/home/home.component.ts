51e854871aa264850e69adcfc62664d45bbde61f
Elisa Albertini <elisa.albertini25@gmail.com>
2024-08-05 18:12:53 +0200

    chore: add error management and cart button

--- customer-application/src/app/home/home.component.ts
+++ customer-application/src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'home',
   standalone: true,
   imports: [MatToolbarModule,
-    RouterOutlet],
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- customer-application/src/app/menu/menu.component.ts
+++ customer-application/src/app/menu/menu.component.ts
@@ -3,11 +3,15 @@ import { ItemCardComponent } from "../item-card/item-card.component";
 import { Item } from '../../utils/Item';
 import { MenuServiceMessages, RequestMessage, ResponseMessage } from '../../utils/message';
 import { Service } from '../../utils/service';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [ItemCardComponent],
+  imports: [ItemCardComponent,
+    CommonModule,
+    MatProgressSpinnerModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
@@ -15,6 +19,9 @@ export class MenuComponent {
 
   ws!: WebSocket
   items: Item[] = []
+  errorMsg = ""
+  display = false
+  error = false
 
   constructor() {
     this.ws = new WebSocket('ws://localhost:3000')
@@ -28,20 +35,34 @@ export class MenuComponent {
     }
 
     this.ws.onerror = () => {
+      this.display = true
+      this.error = true
+      this.errorMsg = "Server not available!"
       this.ws.close()
     }
 
-    this.ws.onopen = function(e) {
-      this.send(JSON.stringify(request))
+    this.ws.onopen = () => {
+      this.ws.send(JSON.stringify(request))
     }
 
-    this.ws.onmessage = function(e) {
+    this.ws.onmessage = (e) => {
       let data = JSON.parse(e.data) as ResponseMessage
-      setItems(JSON.parse(data.data))
-    }
-    const setItems = (items: []) => {
-      this.items = items
+      this.display = true
+      switch (data.code) {
+        case 200:
+          this.items = JSON.parse(data.data)
+          break
+        case 404:
+          this.setError("Warehouse empty!")
+          break
+        case 500:
+          this.setError("Microservice not available!")
+      }
     }
   }
 
+  setError(msg: string) {
+    this.error = true
+    this.errorMsg = msg
+  }
 }
