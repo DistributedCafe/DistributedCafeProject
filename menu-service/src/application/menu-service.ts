24c49484cfeb7e637c399f74103f21708bd4aa71
Elisa Albertini <elisa.albertini25@gmail.com>
2024-08-14 10:57:55 +0200

    refactor: reduce code duplication

--- menu-service/src/application/menu-service.ts
+++ menu-service/src/application/menu-service.ts
@@ -1,11 +1,11 @@
-import { MenuMessage } from "../../menu-message";
-import { IngredientInRecipe, Item } from "../domain/item";
-import * as repository from "../repository/repository";
+import { MenuMessage } from "../../menu-message"
+import { IngredientInRecipe, Item } from "../domain/item"
+import * as repository from "../repository/repository"
 
 /**
  * This type represents the Response given by the Service. It consists of the generic data and an MenuMessage
  */
-type ServiceResponse<T> = { data?: T, message: MenuMessage };
+type ServiceResponse<T> = { data?: T, message: MenuMessage }
 
 /**
  * Service functionality to add a new item given its information:
