91a40a060a065c001a9e397c5bf4cdbee2a5eb5d
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2024-08-21 10:26:09 +0200

    docs: add Request and Response documentation

--- EmployeeApplication/src/main/java/application/Response.java
+++ EmployeeApplication/src/main/java/application/Response.java
@@ -3,23 +3,39 @@ package application;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 
+/**
+ * This class represents the response, a message received from the server.
+ */
 public class Response {
 
-    int OK_CODE = 200;
-    String ERROR_MESSAGE_SERVER = "ERROR_SERVER_NOT_AVAILABLE";
-
+    /**
+     * It says if the server response is code based on the code.
+     * @return true if the code is ok, false otherwise
+     */
     public boolean isCodeOk(){
-        return code == OK_CODE;
+        return code == 200;
     }
 
+    /**
+     * It says if the server is not available based on the message of the response.
+     * @return true if the server is not available, false otherwise
+     */
     public boolean isServerNotAvailable(){
-        return msg.equals(ERROR_MESSAGE_SERVER);
+        return msg.equals("ERROR_SERVER_NOT_AVAILABLE");
     }
 
+    /**
+     *
+     * @return the response message
+     */
     public String getMsg() {
         return msg;
     }
 
+    /**
+     *
+     * @return the response data
+     */
     public String getData() {
         return data;
     }
@@ -28,6 +44,10 @@ public class Response {
     private final int code;
     private final String data;
 
+    /**
+     * Class constructor that create a Response reading the buffer
+     * @param buffer of the websocket connected to the server
+     */
     public Response(Buffer buffer) {
         JsonObject res = buffer.toJsonObject();
         msg = (String) res.getValue("message");
