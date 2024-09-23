0125324bf6de00c58ee4cefb1b62891c08186596
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2024-09-23 18:16:52 +0200

    chore: add environment variables for axios connections

--- server/src/utils/axios.ts
+++ server/src/utils/axios.ts
@@ -1,28 +1,27 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 import { checkErrorMessage } from './handlers'
 import WebSocket from 'ws'
 
 /**
  * Connection with the warehouse microservice
  */
-export const http = axios.create({
-	baseURL: 'http://warehouse-service:8080'
-})
+export const http = createAxios(process.env.WAREHOUSE_ADDRESS, '8080')
 
 /**
  * Connection with the orders microservice
  */
-export const httpOrders = axios.create({
-	baseURL: 'http://orders-service:8090'
-})
+export const httpOrders = createAxios(process.env.ORDER_ADDRESS, '8090')
 
 /**
  * Connection with the menu microservice
  */
-export const httpMenu = axios.create({
-	baseURL: 'http://menu-service:8085'
-})
+export const httpMenu = createAxios(process.env.MENU_ADDRESS, '8085')
 
+function createAxios(address: string | undefined, port: string): AxiosInstance {
+	return axios.create({
+		baseURL: 'http://' + (address != undefined ? address : 'localhost') + ':' + port
+	})
+}
 /**
  * This function catches the error of a promise and sends a message
  * @param promise 
